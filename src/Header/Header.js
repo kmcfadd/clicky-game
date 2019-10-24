3fc44187a75c27fa82664f672017ef6db93052c2
kmcfadd <39887165+kmcfadd@users.noreply.github.com>
2019-10-24 12:31:41 -0700

    starter files with some header/footer styling

---
+++ src/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css'
+
+function Header () {
+
+    return (
+        <div>
+            <h1>The Game That Never Ends</h1>
+            <h3>click an image to begin <span>score 0 </span> </h3>
+            
+        </div>  
+    )
+
+}
+
+export default Header;
