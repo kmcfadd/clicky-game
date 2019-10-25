8d3e578380139fd17ed1dfc169bff8759c4917d0
kmcfadd <39887165+kmcfadd@users.noreply.github.com>
2019-10-25 14:45:29 -0700

    create functions to shuffle the array and begin game logic, trouble getting state to actually change

--- src/App.js
+++ src/App.js
@@ -8,30 +8,70 @@ class App extends Component {
 
   state = {
     pieces: [
-      { id: '1', name: 'Kyle' },
-      { id: '2', name: 'Ghett' },
-      { id: '3', name: 'Will' },
-      { id: '4', name: 'Travis' },
-      { id: '5', name: 'Stephen' },
-      { id: '6', name: 'Ed' },
-      { id: '7', name: 'Ryan' },
-      { id: '8', name: 'Geno' },
-      { id: '9', name: 'Frank' },
-      { id: '10', name: 'Brian' },
-      { id: '11', name: 'Connor' },
-      { id: '12', name: 'Rebecca' }
-    ]
+      { id: '0', name: 'Kyle', clicked: false },
+      { id: '1', name: 'Ghett', clicked: false },
+      { id: '2', name: 'Will', clicked: false },
+      { id: '3', name: 'Travis', clicked: false },
+      { id: '4', name: 'Stephen', clicked: false },
+      { id: '5', name: 'Ed', clicked: false },
+      { id: '6', name: 'Ryan', clicked: false },
+      { id: '7', name: 'Geno', clicked: false },
+      { id: '8', name: 'Frank', clicked: false },
+      { id: '9', name: 'Brian', clicked: false },
+      { id: '10', name: 'Connor', clicked: false },
+      { id: '11', name: 'Rebecca', clicked: false }
+    ],
+    score: 0,
+    topScore: 0
+  }
+
+  onClick (event) {
+    event.preventDefault();
+
+    this.arrayShuffle(this.state.pieces)
+    this.gameLogic()
+  }
+
+  arrayShuffle (arr) {
+    let currentIndex = arr.length, tempVal, randomIndex;
+
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      tempVal = arr[currentIndex];
+      arr[currentIndex] = arr[randomIndex];
+      arr[randomIndex] = tempVal;
+    }
+    this.setState( { pieces: arr } )
+  }
+
+  gameLogic (index) {
+
+    const pieces = this.state.pieces
+
+    if (pieces[index].clicked === true) {
+      this.setState( { score: 0 } )
+    }
+    if (pieces[index].clicked === false) {
+      this.setState( { score: +1, topScore: +1 } )
+    }
   }
 
   render () {
 
     return (
       <div className="App">
-        <Header />
+        <Header 
+        score={this.state.score}
+        topScore={this.state.topScore} />
         {this.state.pieces.map((piece, index) => {
           return <Piece 
             key={piece.id}
-            name={piece.name} />
+            name={piece.name}
+            clicked={piece.clicked}
+            onClick={() => this.gameLogic(index)} 
+            />
         })} 
         <Footer />
       </div>
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Header.css'
 
-function Header () {
+function Header ( props ) {
 
     return (
         <div>
             <h1>The Game That Never Ends</h1>
-            <h3>click an image to begin <span>score 0 </span> </h3>
+            <h3>click an image to begin <span>score {props.score} | topScore {props.topScore}</span> </h3>
             
         </div>  
     )
