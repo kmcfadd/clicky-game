6f5898e7b7fbf24da786ffe8dfd322f6fc7a3657
kmcfadd <39887165+kmcfadd@users.noreply.github.com>
2019-10-26 17:51:23 -0700

    Updates

---
+++ precache-manifest.59d9421344cb5f56e70ff0752d1c73cf.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37aa2d39401398bd16786ad6209ec6b3",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "9f45af8d80e2999d8d90",
+    "url": "/clicky-game/static/css/main.9c56e422.chunk.css"
+  },
+  {
+    "revision": "b79a6ea6e4aba7a6b5d8",
+    "url": "/clicky-game/static/js/2.5943ade0.chunk.js"
+  },
+  {
+    "revision": "9f45af8d80e2999d8d90",
+    "url": "/clicky-game/static/js/main.fcd428c2.chunk.js"
+  },
+  {
+    "revision": "9e63f44ec9a3dfc4c648",
+    "url": "/clicky-game/static/js/runtime-main.742e30a5.js"
+  },
+  {
+    "revision": "4cf8c72ed09a787eff8d5f1b18dea964",
+    "url": "/clicky-game/static/media/alex.4cf8c72e.png"
+  },
+  {
+    "revision": "7eb6b400c2df2b037330223f300ad3b3",
+    "url": "/clicky-game/static/media/dva.7eb6b400.png"
+  },
+  {
+    "revision": "f561e6b61b7cda47e6aee9eb83986964",
+    "url": "/clicky-game/static/media/heroesbg.f561e6b6.jpg"
+  },
+  {
+    "revision": "597b85f68749903d81cf2fbd84db0574",
+    "url": "/clicky-game/static/media/jaina.597b85f6.png"
+  },
+  {
+    "revision": "fec191ecb99fcb2ebbde9febe03069df",
+    "url": "/clicky-game/static/media/lili.fec191ec.png"
+  },
+  {
+    "revision": "ab96da34b8ba8df71d855ec767657acb",
+    "url": "/clicky-game/static/media/liming.ab96da34.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/clicky-game/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "f46ddd60b740cae3cab0a36a4bc31b59",
+    "url": "/clicky-game/static/media/lunara.f46ddd60.png"
+  },
+  {
+    "revision": "e088911c12382078395999d94e69efa3",
+    "url": "/clicky-game/static/media/maiev.e088911c.png"
+  },
+  {
+    "revision": "f1982277f97967b53e1c4b2528715644",
+    "url": "/clicky-game/static/media/sonya.f1982277.png"
+  },
+  {
+    "revision": "7914044791a49dddf2fb017ba9352496",
+    "url": "/clicky-game/static/media/sylvanas.79140447.png"
+  },
+  {
+    "revision": "b032df80a4c485fd637aa04a351c0aa2",
+    "url": "/clicky-game/static/media/tracer.b032df80.png"
+  },
+  {
+    "revision": "b0e9e552a87327edb2702c9f564ac142",
+    "url": "/clicky-game/static/media/tyrande.b0e9e552.png"
+  },
+  {
+    "revision": "767c584fb8b54c47ca8da78551237b72",
+    "url": "/clicky-game/static/media/valla.767c584f.png"
+  },
+  {
+    "revision": "537a17adec18123825fa8373c775f2d6",
+    "url": "/clicky-game/static/media/whitemane.537a17ad.png"
+  },
+  {
+    "revision": "afe4278e9df00354f9afe3a4324bbe3e",
+    "url": "/clicky-game/static/media/yrel.afe4278e.png"
+  },
+  {
+    "revision": "a6abb5a011263a5f4c722033c83cbd1f",
+    "url": "/clicky-game/static/media/zagara.a6abb5a0.png"
+  }
+]);
